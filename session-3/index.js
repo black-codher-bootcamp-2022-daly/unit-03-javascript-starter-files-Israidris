4724246d34efe3379327e67b390583c216583afd
Israidris <isra_no@hotmail.com>
2022-10-10 21:09:37 +0100

    futureAge calculation

--- session-3/index.js
+++ session-3/index.js
@@ -13,20 +13,28 @@ let c = a + b;
 const thisIsANumber = 18;
 let THIS_IS_A_NUMBER = 10;
 
-function sayHey() {
-    console.log('hey ' + userName)
+function sayHey(name = 'Visual ', surname = 'Studio Code') {
+    console.log('hey ' + name + surname)
 };
 
 sayHey();
 
 function conversation() {
-    sayHey()
+    sayHey('Isra ', 'Idris')
     console.log('How are you?')
     console.log('Goodbye')
 };
 
 conversation();
 
+sayHey('Black ', 'Codher')
+sayHey('Tobia ', 'Donati')
 
+function futureAge(firstName, currentAge) {
+    const calculatedAge  = currentAge + 5;
+    return "Hi " + firstName + " you will be " + calculatedAge + " years old in 5 years";
+}
+
+console.log(futureAge('Isra', 28))
 
 
