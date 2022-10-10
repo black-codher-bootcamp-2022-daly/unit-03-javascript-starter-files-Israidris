34804e34221d342fdb0a6fabfa09aed82b3ac79c
Israidris <isra_no@hotmail.com>
2022-10-10 19:56:21 +0100

    function

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,32 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const userName ='Isra';
+let age = '28';
+
+let a = 5;
+let b = 10;
+
+console.log(a + b);
+
+let c = a + b;
+
+const thisIsANumber = 18;
+let THIS_IS_A_NUMBER = 10;
+
+function sayHey() {
+    console.log('hey ' + userName)
+};
+
+sayHey();
+
+function conversation() {
+    sayHey()
+    console.log('How are you?')
+    console.log('Goodbye')
+};
+
+conversation();
+
+
+
+
